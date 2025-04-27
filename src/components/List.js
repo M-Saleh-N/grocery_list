f424e89befdfe2af4972d436cd5300e7fabd8f67
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-27 10:41:20 +0300

    added a delete button in the html

--- src/components/List.js
+++ src/components/List.js
@@ -16,6 +16,15 @@ function GroceryList() {
     setChecked((prev) => ({ ...prev, [item]: !prev[item] }));
   };
 
+  const deleteItem = (itemToDelete) => {
+    setItems(items.filter((item) => item !== itemToDelete));
+    setChecked((prev) => {
+      const newChecked = { ...prev };
+      delete newChecked[itemToDelete];
+      return newChecked;
+    });
+}
+
   return (
     <div className="grocery-container">
       <h1>🛒 Grocery List</h1>
@@ -38,6 +47,7 @@ function GroceryList() {
               onChange={() => toggleCheck(item)}
             />
             <span className={checked[item] ? "checked" : ""}>{item}</span>
+            <button className="delete-btn" onClick={() => deleteItem(item)}>Delete</button>
           </li>
         ))}
       </ul>
