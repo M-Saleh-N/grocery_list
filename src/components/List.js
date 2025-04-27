55b84aabe638a4ac40eccf4789fa6ecd6451e39f
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-27 10:33:19 +0300

    added conditions

---
+++ src/components/List.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import './list.css';
+
+function GroceryList() {
+  const [items, setItems] = useState([]);
+  const [checked, setChecked] = useState({});
+  const [input, setInput] = useState("");
+}
+
+export default GroceryList;
